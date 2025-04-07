6950480895e17f51982db48612f5f6a59bd52f96
mmimie412 <mmimie412@gmail.com>
2025-04-07 21:48:02 +0100

    update code

--- ansible/index.js
+++ ansible/index.js
@@ -2,13 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
-mongoose.connect('mongodb://localhost/devops', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://172.31.94.105:27017/devops', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const Dev = mongoose.model('Dev', { name: String });
 
 app.get('/', async (req, res) => {
   const devs = await Dev.find();
   const names = devs.map(d => `<li>${d.name}</li>`).join('');
+  const message = "<p>This pipeline was built by Mildred</p>";
   res.send(`<h1>I’m building pipelines like a pro!</h1><ul>${names}</ul>`);
 });
 
--- ansible/init.js
+++ ansible/init.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/devops', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://172.31.94.105:27017/devops', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const Dev = mongoose.model('Dev', { name: String });
 
-Dev.insertMany([{ name: 'Team A' }, { name: 'Team B' }, { name: 'Team C' }])
+Dev.insertMany([{ name: 'Team 28' }, { name: 'Team B' }, { name: 'Team C' }])
   .then(() => {
     console.log('Database seeded successfully');
     process.exit();
